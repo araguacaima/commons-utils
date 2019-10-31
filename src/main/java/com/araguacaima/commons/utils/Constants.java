f2a78fc3b18d3dd97d99f416a1f5394b596685d6
Alejandro Manuel Méndez Araguacaima <argauacaima@gmail.com>
2019-10-31 10:26:53 -0600

    Publishing release 1.1

--- src/main/java/com/araguacaima/commons/utils/Constants.java
+++ src/main/java/com/araguacaima/commons/utils/Constants.java
@@ -10,52 +10,8 @@ import java.util.Locale;
 public class Constants {
     public static final String SIMPLE = "Simple";
     public static final String SHARED = "Shared";
-    public static final String BASE_API_NAMESPACE = "https://www.bbvaapis.com/";
-    public static final String BASE_API_NAMESPACE_PATTERN = BASE_API_NAMESPACE + "${serviceName}/${version}";
     public static final String API_BASE_NAME = "api.raml";
     public static final String COMPLETE_TEXT = "$$$COMPLETE$$$";
-    public static final String APIS_COMMONS_COMMONS = "glapi-global-apis-commons-commons";
-    public static final Locale LOCALE_EN = new Locale("en", "us");
-    public static final Locale LOCALE_ES = new Locale("es", "es");
-    public static Collection<Locale> LOCALES = new ArrayList<Locale>() {
-        {
-            add(LOCALE_EN);
-            add(LOCALE_ES);
-        }
-    };
-
-    public enum SOURCE_TYPE {
-        LOCAL,
-        GOOGLE_DRIVE,
-        REMOTE
-    }
-
-    public enum DROOLS_SESSION_TYPE {
-        STATELESS,
-        STATEFULL
-    }
-
-    public enum URL_RESOURCE_STRATEGIES {
-        WORKBENCH,
-        MAVEN,
-        ABSOLUTE_DECISION_TABLE_PATH,
-        GOOGLE_DRIVE_DECISION_TABLE_PATH,
-        ABSOLUTE_DRL_PATH
-    }
-
-    public enum RULES_SESSION_TYPE {
-        STATEFUL,
-        STATELESS
-    }
-
-    public enum RULES_REPOSITORY_STRATEGIES {
-        DRL,
-        DECISION_TABLE
-    }
-
-    public enum GOOGLE_DRIVE_CREDENTIALS_STRATEGIES {
-        SERVER_TO_SERVER
-    }
 
     public enum UrlParams {
         PATH,
@@ -152,8 +108,8 @@ public class Constants {
     }
 
     public enum JaxRsGenerationStyle {
-        ASO("Aso"),
-        APX("Apx");
+        TypeA("TypeA"),
+        TypeB("TypeB");
 
         final String value;
 
